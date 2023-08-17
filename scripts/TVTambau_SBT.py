d5f26dc2bb386c31a73695675f47b179a480b6d4
Luiz Carlos Devilarte <lcdevilarte@hotmail.com>
2023-08-17 04:30:33 -0300

    Update URLs

--- scripts/TVTambau_SBT.py
+++ scripts/TVTambau_SBT.py
@@ -14,13 +14,13 @@ def grab(url):
         response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('../assets/offline.m3u8')
+                print('/home/runner/work/YTBLive_Catcher/YTBLive_Catcher/assets/offline.m3u8')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('../assets/offline.m3u8')
+                print('/home/runner/work/YTBLive_Catcher/YTBLive_Catcher/assets/offline.m3u8')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
@@ -42,7 +42,7 @@ print('#EXTM3U')
 print('#EXT-X-VERSION:3')
 print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
 s = requests.Session()
-with open('../sources/TVTambau_SBT.txt') as f:
+with open('/home/runner/work/YTBLive_Catcher/YTBLive_Catcher/sources/TVTambau_SBT.txt') as f:
     for line in f:
         line = line.strip()
         if not line or line.startswith('~~'):
